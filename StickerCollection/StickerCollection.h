512ad168408104e7e5cf3c3bc18d324f734782fe
Alexander Gavrilko <gavrilko.alexander@gmail.com>
2019-03-17 11:26:58 +0200

    Initial Commit

---
+++ StickerCollection/StickerCollection.h
@@ -0,0 +1,19 @@
+//
+//  StickerCollection.h
+//  StickerCollection
+//
+//  Created by Alexander Gavrilko on 3/17/19.
+//  Copyright © 2019 Alexander Gavrilko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StickerCollection.
+FOUNDATION_EXPORT double StickerCollectionVersionNumber;
+
+//! Project version string for StickerCollection.
+FOUNDATION_EXPORT const unsigned char StickerCollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StickerCollection/PublicHeader.h>
+
+
